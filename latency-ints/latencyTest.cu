d743ce66388f3b7336a633c30b08d7d229b4581e
Mohammad Dashti <mdashti@sfu.ca>
2014-12-10 22:00:05 +0000

    various fixes for latency and add latency-ints

--- latency-ints/latencyTest.cu
+++ latency-ints/latencyTest.cu
@@ -27,37 +27,38 @@ inline double diff_s(struct timeval start, struct timeval end)
    return ((double) (end.tv_usec - start.tv_usec) / 1000000 + (double) (end.tv_sec - start.tv_sec));
 }
 
-__global__ void readKernel(unsigned char *memory, unsigned char *memoryToRead)
+__global__ void readKernel(int *memory, int *memoryToRead)
 {
    int tid = threadIdx.x + blockIdx.x*blockDim.x;
    //memory[tid]=memoryToRead[tid];
-   //__shared__ unsigned char temp; 
-   unsigned char temp = memoryToRead[tid];
+   //__shared__ int temp; 
+   int temp = memoryToRead[tid];
    if(!temp)
       __syncthreads();
 }
-__global__ void writeKernel(unsigned char *memory)
+__global__ void writeKernel(int *memory)
 {
    int tid = threadIdx.x + blockIdx.x*blockDim.x;
    memory[tid]=5;
+   //memory[tid]++;
 }
 __global__ void nullKernel(int *memory)
 {
 
 }
-__global__ void initCudaMallocd(unsigned char *memory, int N)
+__global__ void initCudaMallocd(int *memory, int N)
 {
    int tid =threadIdx.x;
    if(tid==0){
-      for(int k=0;k< N ;k++)
+      for(int k=0;k< N*N/(16*16) ;k++)
          memory[k]=5;
    }
 }
 
-void verify(unsigned char* memory, int N)
+void verify(int* memory, int N)
 {
    int error = 0;
-   for(int i =0; i<N; i++){
+   for(int i =0; i<N*N/(16*16); i++){
       if(memory[i]!=5){
          error = 1;
          break;
@@ -68,12 +69,12 @@ void verify(unsigned char* memory, int N)
    else
       printf("verified SUCCESS\n");
 }
-__global__ void verifyCudaMallocd(unsigned char* memory, int N)
+__global__ void verifyCudaMallocd(int* memory, int N)
 {
    int tid=threadIdx.x;
    if(tid==0) {
       int error = 0;
-      for(int i =0; i<N; i++){
+      for(int i =0; i<N*N/(16*16); i++){
          if(memory[i]!=5){
             error = 1;
             break;
@@ -90,9 +91,9 @@ __global__ void verifyCudaMallocd(unsigned char* memory, int N)
    int
 main( int argc, char *argv[] )
 {
-    unsigned char *hostAllocd, *cudaMallocd, *cpuMallocd;
+    int *hostAllocd, *cudaMallocd, *cpuMallocd;
     int ITERATIONS = 100000;
-    int numBytes = 1;
+    int numBytes = 1024;
     struct timeval  tv1, tv2;
     int opt;
     int read=0; //read benchmark? or write?
@@ -120,24 +121,37 @@ main( int argc, char *argv[] )
    }
 
 
-   cpuMallocd = (unsigned char *)malloc(sizeof(unsigned char)*numBytes);
+   cpuMallocd = (int *)malloc(sizeof(int)*numBytes*numBytes/(16*16));
    assert(cpuMallocd);
-   HANDLE_ERROR( cudaHostAlloc( &hostAllocd, sizeof(unsigned char)*numBytes, 0 ) );
+   HANDLE_ERROR( cudaHostAlloc( &hostAllocd, sizeof(int)*numBytes*numBytes/(16*16), 0 ) );
    for(int k=0;k< numBytes ;k++){
       cpuMallocd[k]=1;
       hostAllocd[k]=1;
    }
 
-   HANDLE_ERROR( cudaMalloc( &cudaMallocd, sizeof(unsigned char)*numBytes) );
-   HANDLE_ERROR( cudaMemcpy( cudaMallocd,hostAllocd, sizeof(unsigned char)*numBytes,cudaMemcpyDefault) );
+   HANDLE_ERROR( cudaMalloc( &cudaMallocd, sizeof(int)*numBytes) );
+   HANDLE_ERROR( cudaMemcpy( cudaMallocd,hostAllocd, sizeof(int)*numBytes,cudaMemcpyDefault) );
 
-   int num_of_blocks=1;
-   int num_of_threads_per_block=numBytes;
-   if(numBytes>1024){
-      num_of_blocks = 16;
-      num_of_threads_per_block = numBytes/16;
-   }
-
+   //int num_of_blocks=1;
+   //int num_of_threads_per_block=numBytes;
+   //if(numBytes>1024){
+   //   num_of_blocks = 16;
+   //   num_of_threads_per_block = numBytes/16;
+   //}
+   int num_of_blocks,num_of_threads_per_block;
+  // if(numBytes==1)
+  // {
+  //    num_of_blocks=1;
+  //    num_of_threads_per_block=1;
+  // }
+  // else
+  // {
+  //    assert(numBytes%2==0);
+  //    num_of_blocks=2;
+  //    num_of_threads_per_block=numBytes/2;
+  // }
+   num_of_blocks=numBytes/16;
+   num_of_threads_per_block=numBytes/16;
    //HANDLE_ERROR(cudaDeviceReset());  //this causes kernel launch failure!! check with cuda-memcheck
    HANDLE_ERROR(cudaFree(0));
    switch (benchmarkType) {
@@ -145,9 +159,9 @@ main( int argc, char *argv[] )
                  if(read)
                  {
 
-                    unsigned char *memoryToRead;
-                    HANDLE_ERROR( cudaHostAlloc( &memoryToRead, sizeof(unsigned char)*numBytes, 0 ) );
-                    for(int k=0;k< numBytes ;k++)
+                    int *memoryToRead;
+                    HANDLE_ERROR( cudaHostAlloc( &memoryToRead, sizeof(int)*numBytes*numBytes/(16*16), 0 ) );
+                    for(int k=0;k< numBytes*numBytes/(16*16) ;k++)
                        memoryToRead[k]=5;
                     gettimeofday(&tv1, NULL);
                     for(int i = 0; i < ITERATIONS; i++) {
@@ -166,11 +180,11 @@ main( int argc, char *argv[] )
                        HANDLE_ERROR( cudaDeviceSynchronize());
                     }
                     gettimeofday(&tv2, NULL);
-                    verify(hostAllocd,numBytes);
+                    //verify(hostAllocd,numBytes);
                  }
                  HANDLE_ERROR( cudaGetLastError());
                  double elapsedTimeSeconds = diff_s(tv1,tv2);
-                 printf("HostAlloc [%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
+                 printf("[%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
                  break;
               }
    
@@ -178,8 +192,8 @@ main( int argc, char *argv[] )
                  if(read)
                  {
 
-                    unsigned char *memoryToRead;
-                    HANDLE_ERROR( cudaMalloc( &memoryToRead, sizeof(unsigned char)*numBytes ) );
+                    int *memoryToRead;
+                    HANDLE_ERROR( cudaMalloc( &memoryToRead, sizeof(int)*numBytes*numBytes/(16*16) ) );
                     initCudaMallocd<<<1,1>>>(memoryToRead,numBytes);
                     HANDLE_ERROR( cudaDeviceSynchronize());
                     gettimeofday(&tv1, NULL);
@@ -200,12 +214,12 @@ main( int argc, char *argv[] )
                        HANDLE_ERROR( cudaDeviceSynchronize());
                     }
                     gettimeofday(&tv2, NULL);
-                    verifyCudaMallocd<<<1,1>>>(cudaMallocd,numBytes);
-                    HANDLE_ERROR( cudaDeviceSynchronize());
+                    //verifyCudaMallocd<<<1,1>>>(cudaMallocd,numBytes);
+                    //HANDLE_ERROR( cudaDeviceSynchronize());
                  }
                  HANDLE_ERROR( cudaGetLastError());
                  double elapsedTimeSeconds = diff_s(tv1,tv2);
-                 printf("CudaMalloc [%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
+                 printf("[%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
                  break;
               }
 
@@ -220,7 +234,6 @@ main( int argc, char *argv[] )
                  HANDLE_ERROR( cudaGetLastError());
                  double elapsedTimeSeconds = diff_s(tv1,tv2);
                  printf("null kernel launch overhead = %f us\n",elapsedTimeSeconds*1e6/(float)ITERATIONS);
-                 break;
               
               }
       case 3: {//read/Write to cpu mallocd data
@@ -228,13 +241,13 @@ main( int argc, char *argv[] )
                  {
 
                     int temp;
-                    unsigned char *memoryToRead = (unsigned char *)malloc(sizeof(unsigned char)*numBytes );
+                    int *memoryToRead = (int *)malloc(sizeof(int)*numBytes*numBytes/(16*16) );
                     assert(memoryToRead);
-                    for(int k=0;k< numBytes ;k++)
+                    for(int k=0;k< numBytes*numBytes/(16*16) ;k++)
                        memoryToRead[k]=5;
                     gettimeofday(&tv1, NULL);
                     for(int i = 0; i < ITERATIONS; i++) {
-                       for(int j=0; j<numBytes; j++){
+                       for(int j=0; j<numBytes*numBytes/(16*16); j++){
                           temp=memoryToRead[j];
                           if(!temp)
                              cpuMallocd[j]=temp;
@@ -248,14 +261,14 @@ main( int argc, char *argv[] )
                  {
                     gettimeofday(&tv1, NULL);
                     for(int i = 0; i < ITERATIONS; i++) {
-                       for(int k=0;k< numBytes ;k++)
+                       for(int k=0;k< numBytes*numBytes/(16*16) ;k++)
                           cpuMallocd[k]=5;
                     }
                     gettimeofday(&tv2, NULL);
                     verify(cpuMallocd,numBytes);
                  }
                  double elapsedTimeSeconds = diff_s(tv1,tv2);
-                 printf("cpu malloc [%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
+                 printf("[%s] Latency including kernel launch overhead = %f us\n",(read==1)?"read":"write",elapsedTimeSeconds*1e6/(float)ITERATIONS);
                  break;
               }
    

a287e8a9126e49cfe7716d8f9ef87d3e3335b96e
Mohammad Dashti <mdashti@sfu.ca>
2014-12-18 20:37:22 +0000

    add case3.cu and case4.cu

---
+++ latency-64b/case4.cu
@@ -0,0 +1,56 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+static void HandleError( cudaError_t err, const char *file, int line ) {
+    
+    if (err != cudaSuccess) {
+        
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ), file, line );
+        exit( EXIT_FAILURE );
+    
+    }
+
+}
+
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+#define HANDLE_NULL( a ) {if (a == NULL) { \
+                            printf( "Host memory failed in %s at line %d\n", \
+                                    __FILE__, __LINE__ ); \
+                            exit( EXIT_FAILURE );}}
+
+
+   int
+main( int argc, char *argv[] )
+{
+   int ITERATIONS = 1;
+   int numBytes = 1024;
+
+   uint64_t *memory_to_access;
+   HANDLE_ERROR(cudaHostAlloc(&memory_to_access,sizeof(uint64_t)*numBytes,0));
+   for(int k=0;k< numBytes ;k++)
+      memory_to_access[k]=5;
+   printf("Press enter to continue...\n");
+   getchar();
+             
+   uint64_t fake=0;
+   for(int i=0; i<ITERATIONS; i++) {
+      for (int j = 0; j < (numBytes); j += 8) {
+         fake += memory_to_access[j];
+         fake += memory_to_access[j + 1];
+         fake += memory_to_access[j + 2];
+         fake += memory_to_access[j + 3];
+         fake += memory_to_access[j + 4];
+         fake += memory_to_access[j + 5];
+         fake += memory_to_access[j + 6];
+         fake += memory_to_access[j + 7];
+      }
+   }
+   printf("Press enter to continue...\n");
+   getchar();
+    
+   cudaFreeHost(memory_to_access);
+
+   return 0;
+}

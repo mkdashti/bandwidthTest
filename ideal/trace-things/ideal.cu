d6d06f0f22830cc4f5a28fcb7b4728c7f2459ec2
Mohammad Dashti <mdashti@sfu.ca>
2015-02-19 12:10:53 -0800

    update trace-things ideal.cu code

--- ideal/trace-things/ideal.cu
+++ ideal/trace-things/ideal.cu
@@ -29,13 +29,24 @@ inline double diff_s(struct timeval start, struct timeval end)
    return ((double) (end.tv_usec - start.tv_usec) / 1000000 + (double) (end.tv_sec - start.tv_sec));
 }
 
-__global__ void kernel(uint64_t *in, uint64_t *out)
+__global__ void kernel(uint64_t *in, uint64_t *out, int threads)
 {
    int tid = threadIdx.x + blockIdx.x*blockDim.x;
    //in = (uint64_t *)malloc(sizeof(uint64_t));
    //out = (uint64_t *)malloc(sizeof(uint64_t));
-   out[tid] = in[tid]+tid;
+   if(tid < threads)
+      out[tid] = in[tid]+tid;
 }
+__global__ void kernel_d(uint64_t *in, uint64_t *out, int threads)
+{
+   int tid = threadIdx.x + blockIdx.x*blockDim.x;
+   //in = (uint64_t *)malloc(sizeof(uint64_t));
+   //out = (uint64_t *)malloc(sizeof(uint64_t));
+   if(tid < threads)
+      out[tid] = in[tid]+tid;
+}
+
+
 __global__ void nullKernel(void)
 {
 
@@ -62,32 +73,28 @@ void cpu_compute(uint64_t *in, uint64_t *out, int numBytes)
 int main( int argc, char *argv[] )
 {
     uint64_t *in, *out, *in_d, *out_d;
-    int numBytes = 1;
     int opt;
-    int benchmarkType = 0;
+    int iterations = 1;
+    int blocks = 1;
+    int threads = 1;
 
-    while ((opt = getopt(argc, argv, "m:b:")) != -1) {
+    while ((opt = getopt(argc, argv, "b:t:i:")) != -1) {
        switch (opt) {
-          case 'm':
-             numBytes = atoi(optarg);
+          case 'b':
+             blocks = atoi(optarg);
              //assert(numBytes%16 == 0 && numBytes<=1024);
              break;
-          case 'b':
-             benchmarkType = atoi(optarg);
+          case 'i':
+             iterations = atoi(optarg);
              break;
-
+          case 't':
+             threads = atoi(optarg);
+             break;
           default: /* '?' */
              break;
        }
     }
 
-
-    int num_of_blocks=1;
-    int num_of_threads_per_block=numBytes;
-    if(numBytes>1024){
-       num_of_blocks = 1024;
-       num_of_threads_per_block = numBytes/1024;
-    }
 
     HANDLE_ERROR(cudaFree(0));
     //cudaHostAlloc(&in,numBytes*sizeof(uint64_t),0);
@@ -103,41 +110,42 @@ int main( int argc, char *argv[] )
     //printf("managed %p\n",out_d);
 
 
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
 
-    cudaHostAlloc(&in,numBytes*sizeof(uint64_t),0);
-    cudaHostAlloc(&out,numBytes*sizeof(uint64_t),0);
+    cudaHostAlloc(&in,blocks*threads*sizeof(uint64_t),0);
+    cudaHostAlloc(&out,blocks*threads*sizeof(uint64_t),0);
    
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
 
-    cudaMallocManaged(&in_d,numBytes*sizeof(uint64_t));
-    cudaMallocManaged(&out_d,numBytes*sizeof(uint64_t));
+    cudaMallocManaged(&in_d,blocks*threads*sizeof(uint64_t));
+    cudaMallocManaged(&out_d,blocks*threads*sizeof(uint64_t));
 
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
 
-    kernel<<<num_of_blocks,num_of_threads_per_block>>>(in,out);
+    for(int i = 0; i<iterations; i++) {
+       kernel<<<blocks,threads>>>(in,out,blocks*threads);
+       cudaDeviceSynchronize();
+       kernel_d<<<blocks,threads>>>(in_d,out_d,blocks*threads);
+       cudaDeviceSynchronize();
+    }
 
-    printf("Press enter to continue...\n");
-    getchar();
+    cpu_compute(in,out,blocks*threads);
 
-    cudaDeviceSynchronize();
-    cpu_compute(in,out,numBytes);
-
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
     cudaFreeHost(in);
     cudaFreeHost(out);
     
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
     cudaFree(in_d);
     cudaFree(out_d);
 
-    printf("Press enter to continue...\n");
-    getchar();
+    //printf("Press enter to continue...\n");
+    //getchar();
    
     return 0;
 }
